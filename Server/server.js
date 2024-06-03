6b38716541350518647d7ae53b6072a5b9b61988
gabrielsilva18 <gabriellealmaio1@gmail.com>
2024-06-02 23:24:28 -0300

    Atualização do arquivo server.js

--- Server/server.js
+++ Server/server.js
@@ -5,30 +5,28 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://valentines-day-three.vercel.app/", // Substitua pelo seu domínio real no Vercel
-  optionsSuccessStatus: 200,
-};
+// Configuração do CORS para permitir todas as origens (para testes)
+app.use(cors());
 
-app.use(cors(corsOptions));
+const imagensFolder = path.join(__dirname, "..", "src", "assets", "imagens");
 
-// Caminho das imagens, ajustado para produção
-const imagensFolder = path.join(__dirname, "..", "src", "assets", "imagens"); // Caminho
-
-console.log("Pasta de imagens:", imagensFolder); // Log da pasta de imagens
+console.log("Pasta de imagens:", imagensFolder);
 
 app.get("/listar-imagens", (req, res) => {
+  console.log("Recebida requisição para listar imagens");
   try {
     const files = fs.readdirSync(imagensFolder);
-    console.log("Arquivos encontrados:", files); // Log dos arquivos encontrados
+    console.log("Arquivos encontrados:", files);
     const imageUrls = files.map((file) => `assets/imagens/${file}`);
     res.json(imageUrls);
   } catch (error) {
-    console.error("Erro ao listar imagens:", error); // Log do erro
-    res.status(500).send("Erro ao listar imagens"); // Resposta de erro
+    console.error("Erro ao listar imagens:", error);
+    res.status(500).send("Erro ao listar imagens");
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor Node.js rodando na porta 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor Node.js rodando na porta ${port}`);
 });
